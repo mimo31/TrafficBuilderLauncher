f1b139002754a0fe84cbe12c7ed18abba0e59c9b
mimo31 <weweuuuu@email.cz>
2014-10-23 07:09:25 +0200

    A little bit of data and resources

--- TrafficBuilderLauncher/src/FileDriver.java
+++ TrafficBuilderLauncher/src/FileDriver.java
@@ -1,19 +1,24 @@
-import java.io.IOException;
 import java.nio.file.Files;
 
 
 public class FileDriver  {
-	public static void initializeFiles() throws IOException{
+	public static void initializeFiles() throws Exception{
 		if(Files.exists(Functions.getInGameDirPath(""))){
 			
 		}
 		else{
 			Files.createDirectory(Functions.getInGameDirPath(""));
 			Files.createDirectory(Functions.getInGameDirPath("\\Resources"));
+			Files.createDirectory(Functions.getInGameDirPath("\\Resources\\Default"));
 			Files.createDirectory(Functions.getInGameDirPath("\\Saves"));
 			Files.createDirectory(Functions.getInGameDirPath("\\Game"));
 			Files.createDirectory(Functions.getInGameDirPath("\\SmallData"));
+			getResource("Font.ttf", "Font.ttf");
 		}
 		System.exit(0);
 	}
+	
+	public static void getResource(String InternetName, String LocalName) throws Exception{
+			Functions.writeBytesToFile(Functions.downloadFile("http:\\gjk.cz\\~xfukv01\\TrafficBuilder\\" + InternetName), System.getenv("APPDATA") + "TrafficBuilder\\Resources\\Defualt\\" + LocalName, true);
+		}
 }
